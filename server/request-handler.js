fd28d9012117a767add8bf2272aabea5561b1bed
xfredlix <xfredlix23@yahoo.com>
2017-01-10 10:43:29 -0800

    Add promise to respond to 'POST' and 'GET' requests

--- server/request-handler.js
+++ server/request-handler.js
@@ -18,68 +18,39 @@ var defaultCorsHeaders = {
   'access-control-max-age': 10 // Seconds.
 };
 
+var resultsObj = {results: []};
+
 var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
-
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // // console.logs in your code.
-  // debugger;
-  console.log('Serving request type ' + request.method + ' for url ' + request.body);
-  // console.log(request);
-
-  // The outgoing status.
-  var resultsObj = {results: []};
   var statusCode = 200;
   var headers = defaultCorsHeaders;
 
+  console.log('Serving request type ' + request.method + ' for url ' + request.url);
+
+  headers['Content-Type'] = 'jsonp';
+  if (request.url !== '/classes/messages') {
+    statusCode = 404;   
+    response.writeHead(statusCode, headers);
+    response.end();
+  }
+
   if (request.method === 'POST') {
     statusCode = 201;
+    response.writeHead(statusCode, headers);
+  } else {
+    response.writeHead(statusCode, headers);
+    response.end(JSON.stringify(resultsObj));
   }
-  request.on('data', function(data) {
-    resultsObj.results.push(JSON.parse(data.toString()));
+  var promise = new Promise((resolve, reject) => {
+    request.on('data', (data) => {
+      resolve(JSON.parse(data.toString()));
+    });
   });
-  // See the note below about CORS headers.
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'jsonp';
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
-
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  
-
-  response.end(JSON.stringify(resultsObj));
+  promise.then((data) => {
+    resultsObj.results.push(data);
+    response.end(JSON.stringify(resultsObj));
+  }).catch((err) => {
+    console.error(err);
+  });
 };
 
 module.exports = requestHandler;
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
-
